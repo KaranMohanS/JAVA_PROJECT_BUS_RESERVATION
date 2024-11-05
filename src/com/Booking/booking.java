a837bdd13b7b827fea8192ea51e87400b0f9ca79
karanmohanS <karanmohanjune1702@gmail.com>
2024-11-05 22:02:13 +0530

    tenth  commit

--- src/com/Booking/booking.java
+++ src/com/Booking/booking.java
@@ -1,36 +1,8 @@
 package com.Booking;
-
-public class booking {
     
-    int bookID;
-    int userID;
-    String userName;
-    String userEmail;
-    int busID;
-    String busName;
-    String destination;
-    String seatID;
-    String date;
+public class booking{
+ 
+     int bId;
 
-
-    
-
-
-    public booking(int bookID, int userID, String userName, String userEmail, int busID, String busName,
-        String destination, String seatID) {
-        this.bookID = bookID;
-        this.userID = userID;
-        this.userName = userName;
-        this.userEmail = userEmail;
-        this.busID = busID;
-        this.busName = busName;
-        this.destination = destination;
-        this.seatID = seatID;
-        
-    }
-    
-
-    
-    
-
+     
 }
